114559daa13a2771e77325509cf63b881ffc5b57
Ubuntu <ubuntu@ip-172-31-11-8.eu-north-1.compute.internal>
2023-08-13 09:51:49 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='ML project to demonstrate productionization of ML services — from training and experimenting to model deployment and monitoring.',
+    author='Atharva Kavitkar',
+    license='',
+)
